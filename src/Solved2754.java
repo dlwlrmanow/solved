0853d2be8086bf31c59209be33a730ef2753a150
Yijae <lullulalla9711@gmail.com>
2024-09-14 13:45:35 +0900

    9/14

---
+++ src/Solved2754.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Solved2754 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.nextLine();
+        Character[] arr = grade.chars().mapToObj(c -> (char)c).toArray(Character[]::new); // String을 배열로
+
+        double score = 0;
+
+        if(arr[0].equals('A')){ // arr[0] 먼저 확인
+            score = 4.3;
+        } else if(arr[0].equals('B')){
+            score = 3.3;
+        } else if(arr[0].equals('C')){
+            score = 2.3;
+        } else if(arr[0].equals('D')){
+            score = 1.3;
+        } else if(arr[0].equals('F')){
+            score = 0.0;
+        }
+        if(arr.length == 1) { // arr.length가 1인 F같은 경우 아래 코드 실행없이 바로 출력
+            System.out.println(score);
+            return;
+        }
+
+        if(arr[1].equals('+')){ // arr[1] 확인해서 감점하기
+            score = score;
+        } else if(arr[1].equals('0')){
+            score -= 0.3;
+        } else if(arr[1].equals('-')){
+            score -= 0.6;
+        }
+        System.out.printf("%.1f%n", score); // 없으면 예를들어 C0같은 경우 반올림 없이 2.0의 근사치 출력
+
+        scanner.close();
+    }
+}
---
+++ src/Solved27866.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solved27866 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String S = scanner.nextLine();
+        int i = scanner.nextInt();
+
+        Character[] arr = S.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
+        System.out.println(arr[i - 1]);
+
+        scanner.close();
+    }
+}
