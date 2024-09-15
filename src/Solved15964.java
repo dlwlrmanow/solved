63af619340cd137c1992a5d13449da4338581800
Yijae <lullulalla9711@gmail.com>
2024-09-15 23:38:48 +0900

    9/15

---
+++ src/Solved15964.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Solved {
+    long a;
+    long b;
+
+    void print(long a, long b) {
+        long result = (a + b) * (a - b);
+        System.out.println(result);
+    }
+}
+
+public class Solved15964 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        scanner.close();
+
+        Solved solved = new Solved();
+        solved.print(a, b);
+    }
+}
