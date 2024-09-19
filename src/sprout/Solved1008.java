2e5b7630c1f6b5f27d48bba445221ae76798e15b
Yijae <lullulalla9711@gmail.com>
2024-09-19 20:28:49 +0900

    9/19

--- src/sprout/Solved1008.java
+++ src/sprout/Solved1008.java
@@ -1,17 +1,40 @@
 package sprout;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solved1008 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double A = scanner.nextDouble();
-        double B = scanner.nextDouble();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a, b, c;
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0 && c == 0) break; // 각 입력이 0인 경우 break;
 
-        double result = A / B;
-
-        scanner.close();
-
-        System.out.println(result);
+            if (a > b && a > c) { // 가장 긴 변 찾기
+                int temp = a;
+                a = c;
+                c = temp;
+            }
+            if (b > c && b > a) {
+                int temp = b;
+                b = c;
+                c = temp;
+            }
+            if (a*a + b*b == c*c) {
+                sb.append("right\n");
+            } else {
+                sb.append("wrong\n");
+            }
+        }
+        System.out.print(sb);
     }
 }
---
+++ src/sprout/Solved1978.java
@@ -0,0 +1,40 @@
+package sprout;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solved1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 첫번째 입력 = 배열의 크기로 읽기
+        int N = Integer.parseInt(br.readLine());
+        // N개의 원소를 가진 배열 생성
+        int[] arr = new int[N];
+        // 소수(1과 자기자신만을 약수로 가짐)인 경우 담을 변수
+        int count = 0;
+
+        // 두번째 입력 N개의 Int 배열로 받아오기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); // 차례로 배열에 담고,
+            if(isPrime(arr[i])){ // 자기자신으로 나눠서 나머지 == 0 -> 소수 -> count++
+                count ++;
+            }
+        }
+        System.out.println(count);
+    }
+    public static boolean isPrime(int n) {
+        if(n < 2){
+            return false; // 0,1 은 소수가 아님ㅁ
+        }
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
